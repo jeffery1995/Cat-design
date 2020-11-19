a2f51fb8d61f8f4851687e8fb5baefea7c50d177
jeffery.jiang <jeffery.jiang@crypto.com>
2020-11-19 18:59:21 +0800

    feat: select

---
+++ cat-ui/src/components/Select/Select.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- functions
+    - select provided options
+    - user defined option style
+    - filter
+    - async require options
+   -->
+  <div class="mepo-select">
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
